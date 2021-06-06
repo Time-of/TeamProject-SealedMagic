e3ab721aa63750f41288cd6da8fdeb8feead0552
20181290 한찬희 <78011635+hanchanhui@users.noreply.github.com>
2021-06-07 02:54:48 +0900

    오류

--- 2DSealedmagic/Assets/C#/MainCamera.cs
+++ 2DSealedmagic/Assets/C#/MainCamera.cs
@@ -16,7 +16,7 @@ public class MainCamera : MonoBehaviour
     void LateUpdate()
     {
         transform.position = new Vector3(target.position.x, target.position.y, -10f);
-        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
+        //transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
         //transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
     }
 }
